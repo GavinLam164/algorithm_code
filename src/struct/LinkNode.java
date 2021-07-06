449d8d2b9e3fcf705f2694ca7e6a1546283f8045
GavinLam164 <m13590491852@163.com>
2021-07-06 22:54:16 +0800

    queue

---
+++ src/struct/LinkNode.java
@@ -0,0 +1,26 @@
+package struct;
+
+public class LinkNode {
+    public LinkNode next;
+    public int val;
+
+    public LinkNode(int val) {
+        this.val = val;
+    }
+
+    public static void main(String[] args) {
+        // 链表头节点 1
+        LinkNode head = new LinkNode(1);
+        // 1 -> 2
+        head.next = new LinkNode(2);
+        // 1 -> 2 -> 3
+        head.next.next = new LinkNode(3);
+
+        // 通过头节点遍历链表
+        LinkNode tmp = head;
+        while(tmp != null) {
+            System.out.println(tmp.val);
+            tmp = tmp.next;
+        }
+    }
+}
---
+++ src/struct/Queue.java
@@ -0,0 +1,53 @@
+package struct;
+
+public class Queue {
+
+    public LinkNode head;
+    public LinkNode tail;
+
+    // 入队
+    public void add(int val) {
+        if(head == null) { // 队列为空，新建头节点
+            head = new LinkNode(val);
+            tail = head;
+        }else { // 队列不为空，在尾部追加
+            tail.next = new LinkNode(val);
+            tail = tail.next;
+        }
+    }
+
+    // 出队
+    public int poll() {
+        if(head == null) return -1; // 队列为空，返回-1
+        // 取出头节点的值，并将头节点设置为下一个节点
+        LinkNode next = head.next;
+        int val = head.val;
+        head.next = null;
+        head = next;
+        if(head == null) { // 队列删除后为空，把尾部节点也重置为null
+            tail = null;
+        }
+        return val;
+    }
+
+    // 判断队列是否为空
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        // 依次往队列中添加元素, 即first int
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        // 此时队列元素为 1 -> 2 -> 3
+
+        while(!queue.isEmpty()) { // 队列不为空，则从头部取出元素，即first out
+            System.out.println(queue.poll());
+        }
+    }
+
+}
