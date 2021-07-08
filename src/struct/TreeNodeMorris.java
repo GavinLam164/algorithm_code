83e79f9e9de7fbfb4968c8f899e8b3baf3d15a1c
GavinLam164 <m13590491852@163.com>
2021-07-08 14:56:33 +0800

    tree morris

--- src/struct/TreeNodeMorris.java
+++ src/struct/TreeNodeMorris.java
@@ -128,8 +128,8 @@ public class TreeNodeMorris {
         1	3	4	5	6	7	8
         1	4	3	6	8	7	5
          */
-//        traverse.preOrder(root);
-//        traverse.inOrder(root);
+        traverse.preOrder(root);
+        traverse.inOrder(root);
         traverse.postOrder(root);
     }
 
